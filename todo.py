9c890e463eb4d84c675b858f570187b3471afd14
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2023-10-24 15:01:22 -0400

    Add functionality to program\nAppend and remove from static file to save tasks

--- todo.py
+++ todo.py
@@ -1,3 +1,4 @@
+# Main menu
 def main_menu():
     while True:
         print('[1] Add task')
@@ -17,6 +18,32 @@ def main_menu():
             exit()
         else:
             print('Invalid choice. Please try again.')
+
+# Function to add task to static txt fiile
+def add_task():
+    task = input('Enter your task: ')
+    with open('list.txt', 'a') as list:
+        list.write(f'{task}\n')
+        print(f'Successfully added {task} to list!')
+
+# Function to list out tasks from txt file
+def list_tasks():
+    with open('list.txt', 'r') as list:
+        content = list.read()
+        print(content)
+
+# Function to remove tasks from list by showing tasks
+def remove_task():
+    list_tasks()
+    remove = input('Enter the task you would like to remove: ')
+    with open('list.txt', 'r') as list:
+        content = list.read()
+
+    newlist = content.replace(remove, '')
+
+    with open('list.txt', 'w') as list:
+        list.write(newlist.strip())
+    print(f'Successfully removed {remove} from list.')
 
 if __name__ == "__main__":
     main_menu()
